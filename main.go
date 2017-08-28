cf8a382aee3c3389ca5c420caa8f389e5ef4ea82
David Fandler <d.fandler@javiis.com>
2017-08-28 12:38:53 +0200

    added folder copying

--- main.go
+++ main.go
@@ -46,65 +46,30 @@ func (configs ConfigsModel) validate() error {
 	return nil
 }
 
-// from stackoverflow answer https://stackoverflow.com/questions/21060945/simple-way-to-copy-a-file-in-golang
+func copyFile(source string, dest string) (err error) {
+	sourcefile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
 
-// CopyFile copies a file from src to dst. If src and dst files exist, and are
-// the same, then return success. Otherise, attempt to create a hard link
-// between the two files. If that fail, copy the file contents from src to dst.
-func CopyFile(src, dst string) (err error) {
-	sfi, err := os.Stat(src)
-	if err != nil {
-		return
-	}
-	if !sfi.Mode().IsRegular() {
-		// cannot copy non-regular files (e.g., directories,
-		// symlinks, devices, etc.)
-		return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
-	}
-	dfi, err := os.Stat(dst)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			return
-		}
-	} else {
-		if !(dfi.Mode().IsRegular()) {
-			return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dfi.Name(), dfi.Mode().String())
-		}
-		if os.SameFile(sfi, dfi) {
-			return
-		}
-	}
-	if err = os.Link(src, dst); err == nil {
-		return
-	}
-	err = copyFileContents(src, dst)
-	return
-}
+	defer sourcefile.Close()
 
-// copyFileContents copies the contents of the file named src to the file named
-// by dst. The file will be created if it does not already exist. If the
-// destination file exists, all it's contents will be replaced by the contents
-// of the source file.
-func copyFileContents(src, dst string) (err error) {
-	in, err := os.Open(src)
+	destfile, err := os.Create(dest)
 	if err != nil {
-		return
+		return err
 	}
-	defer in.Close()
-	out, err := os.Create(dst)
-	if err != nil {
-		return
-	}
-	defer func() {
-		cerr := out.Close()
+
+	defer destfile.Close()
+
+	_, err = io.Copy(destfile, sourcefile)
+	if err == nil {
+		sourceinfo, err := os.Stat(source)
 		if err == nil {
-			err = cerr
+			err = os.Chmod(dest, sourceinfo.Mode())
 		}
-	}()
-	if _, err = io.Copy(out, in); err != nil {
-		return
+
 	}
-	err = out.Sync()
+
 	return
 }
 
@@ -118,6 +83,48 @@ func exists(path string) (bool, error) {
 		return false, nil
 	}
 	return true, err
+}
+
+func copyFolder(source string, dest string) (err error) {
+
+	sourceinfo, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+
+	err = os.MkdirAll(dest, sourceinfo.Mode())
+	if err != nil {
+		return err
+	}
+
+	directory, _ := os.Open(source)
+
+	objects, err := directory.Readdir(-1)
+
+	if err != nil {
+		return err
+	}
+
+	for _, obj := range objects {
+
+		sourcefilepointer := source + "/" + obj.Name()
+
+		destinationfilepointer := dest + "/" + obj.Name()
+
+		if obj.IsDir() {
+			err = copyFolder(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				return err
+			}
+		} else {
+			err = copyFile(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				return err
+			}
+		}
+
+	}
+	return
 }
 
 func main() {
@@ -138,7 +145,18 @@ func main() {
 		os.MkdirAll(configs.outputFile, os.ModePerm)
 	}
 
-	err = copyFileContents(configs.inputFile, configs.outputFile)
+	fi, err := os.Stat(configs.inputFile)
+
+	if err != nil {
+		log.Fatalf("Error : %s", err)
+	}
+
+	if fi.IsDir() {
+		err = copyFolder(configs.inputFile, configs.outputFile)
+	} else {
+		err = copyFile(configs.inputFile, configs.outputFile)
+	}
+
 	if err != nil {
 		log.Fatalf("Copying failure: %s", err)
 	}
